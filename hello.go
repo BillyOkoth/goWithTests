debd06c1cb4e0680cf00f5e8850e7f7cde543d45
HenryToti <henryokoth3@gmail.com>
2024-03-26 10:43:20 +0300

    implementation of hello world

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	return getGreetingPrefix(language) + name + "!"
+}
+
+func getGreetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+func main() {
+	fmt.Println(Hello("Chris", ""))
+}
